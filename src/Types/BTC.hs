fd0a8314ab0b2ce68d21fb4a6d8bc8df47ce0a47
Lucas Viana (Bau) <contato@lucasviana.dev>
2021-03-06 23:29:16 -0300

    :art: reorder BTC types

--- src/Types/BTC.hs
+++ src/Types/BTC.hs
@@ -10,13 +10,33 @@ import Data.Text as T
 
 import Generics.Deriving.Base
 
--- | Tipo retornado para moedas
+-- | Data constructor that defines a Currency
 data Currency = Currency 
             { 
               code        :: T.Text
             , rate        :: T.Text
             , description :: T.Text
             , rateFloat   :: Float } deriving Show
+
+-- | Data constructor of response time given by CoinIndex API
+data Time = Time
+            {
+              updated     ::  T.Text
+            , updatedISO  :: T.Text
+            , updatedUk   :: T.Text
+            } deriving (Show, Generic)
+
+-- | Data constructor that defines the Bitcoin Price Index (BPI)
+data BPI = BPI (Map String Currency) deriving (Show, Generic)
+
+-- | Data constructor of CoinIndex API response
+data BTC = BTC
+            {
+                time        :: Time
+              , disclaimer  :: T.Text
+              , bpi         :: BPI
+            } deriving (Show, Generic)
+
 instance FromJSON Currency where
   parseJSON = withObject "Currency" $ \v -> 
     Currency  <$> v .: "code"
@@ -33,13 +53,6 @@ instance ToJSON Currency where
       , "rate_float"  .= rateFloat'
     ]
 
-data Time = Time
-            {
-              updated     ::  T.Text
-            , updatedISO  :: T.Text
-            , updatedUk   :: T.Text
-            } deriving (Show, Generic)
-
 instance FromJSON Time where
   parseJSON = withObject "Time" $ \v -> 
     Time  <$> v .: "updated"
@@ -54,18 +67,8 @@ instance ToJSON Time where
       , "updateduk"   .= updatedUk'
     ]
 
--- | Bitcoin Price Index (BPI)
-newtype BPI = BPI (Map String Currency) deriving (Show, Generic)
-
 instance FromJSON BPI
 instance ToJSON BPI
-
-data BTC = BTC
-            {
-                time        :: Time
-              , disclaimer  :: T.Text
-              , bpi         :: BPI
-            } deriving (Show, Generic)
 
 instance FromJSON BTC where
   parseJSON = withObject "BTC" $ \v -> 
